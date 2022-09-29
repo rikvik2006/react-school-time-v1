60c2869afef91c3e879289fc1d164750f2f94b0b
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-29 22:22:31 +0200

    Setupped react routers dom

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Routes, Route } from "react-router-dom"
+import Home from './page/Home'
+import NavBar from './components/misc/navbar'
+
+function App() {
+    return (
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </>
+    )
+}
+
+export default App
